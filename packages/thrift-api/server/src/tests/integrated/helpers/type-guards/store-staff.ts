2bf1a17192b24a68cd1eb5c0c2738848adef0df4
Daniel Nyong <danielnyong66@gmail.com>
2025-09-22 12:11:04 +0100

    Moved the type guards to tests

---
+++ packages/thrift-api/server/src/tests/integrated/helpers/type-guards/store-staff.ts
@@ -0,0 +1,18 @@
+import {
+  AddStoreStaffSchema,
+  UpdateStoreStaffSchema,
+  StoreStaffListResponseSchema,
+  StoreStaffResponseSchema,
+} from '../../../app-schema/store_staff.js'
+
+export const isValidAddStoreStaffRequest = (data: unknown) =>
+  AddStoreStaffSchema.validate(data).error === undefined
+
+export const isValidUpdateStoreStaffRequest = (data: unknown) =>
+  UpdateStoreStaffSchema.validate(data).error === undefined
+
+export const isValidStoreStaffListResponse = (data: unknown) =>
+  StoreStaffListResponseSchema.validate(data).error === undefined
+
+export const isValidStoreStaffResponse = (data: unknown) =>
+  StoreStaffResponseSchema.validate(data).error === undefined
--- packages/thrift-api/server/src/tests/integrated/store_staff/definitions/index.ts
+++ packages/thrift-api/server/src/tests/integrated/store_staff/definitions/index.ts
@@ -2,37 +2,32 @@ import { StatusCodes } from 'http-status-codes'
 import testRequest from '../../test-request/index.js'
 import { TestRequestWithBody, TestRequest } from '../../test-request/types.js'
 import {
-  AddStoreStaffSchema,
-  UpdateStoreStaffSchema,
-  StoreStaffListResponseSchema,
-  StoreStaffResponseSchema,
-} from '#src/app-schema/store_staff.js'
+  isValidAddStoreStaffRequest,
+  isValidUpdateStoreStaffRequest,
+  isValidStoreStaffListResponse,
+  isValidStoreStaffResponse,
+} from '../../helpers/type-guards/store-staff.js'
 
 const { CREATED, OK, NO_CONTENT, FORBIDDEN } = StatusCodes
 
 export const testAddStaff = (testRequest as TestRequestWithBody)({
   verb: 'post',
   statusCode: CREATED,
-  validateTestReqData: (data: unknown) =>
-    AddStoreStaffSchema.validate(data).error === undefined,
-  validateTestResData: (data: unknown) =>
-    StoreStaffResponseSchema.validate(data).error === undefined,
+  validateTestReqData: isValidAddStoreStaffRequest,
+  validateTestResData: isValidStoreStaffResponse,
 })
 
 export const testListStaff = (testRequest as TestRequest)({
   verb: 'get',
   statusCode: OK,
-  validateTestResData: (data: unknown) =>
-    StoreStaffListResponseSchema.validate(data).error === undefined,
+  validateTestResData: isValidStoreStaffListResponse,
 })
 
 export const testUpdateStaff = (testRequest as TestRequestWithBody)({
   verb: 'put',
   statusCode: OK,
-  validateTestReqData: (data: unknown) =>
-    UpdateStoreStaffSchema.validate(data).error === undefined,
-  validateTestResData: (data: unknown) =>
-    StoreStaffResponseSchema.validate(data).error === undefined,
+  validateTestReqData: isValidUpdateStoreStaffRequest,
+  validateTestResData: isValidStoreStaffResponse,
 })
 
 export const testRemoveStaff = (testRequest as TestRequest)({
@@ -43,8 +38,7 @@ export const testRemoveStaff = (testRequest as TestRequest)({
 export const testAddStaffForbidden = (testRequest as TestRequestWithBody)({
   verb: 'post',
   statusCode: FORBIDDEN,
-  validateTestReqData: (data: unknown) =>
-    AddStoreStaffSchema.validate(data).error === undefined,
+  validateTestReqData: isValidAddStoreStaffRequest,
 })
 
 export const testListStaffForbidden = (testRequest as TestRequest)({
@@ -55,8 +49,7 @@ export const testListStaffForbidden = (testRequest as TestRequest)({
 export const testUpdateStaffForbidden = (testRequest as TestRequestWithBody)({
   verb: 'put',
   statusCode: FORBIDDEN,
-  validateTestReqData: (data: unknown) =>
-    UpdateStoreStaffSchema.validate(data).error === undefined,
+  validateTestReqData: isValidUpdateStoreStaffRequest,
 })
 
 export const testRemoveStaffForbidden = (testRequest as TestRequest)({
