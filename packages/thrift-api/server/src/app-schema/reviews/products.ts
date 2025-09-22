0911517f703ca36c746a24b64f1618b45fdabc0f
Daniel Nyong <danielnyong66@gmail.com>
2025-09-22 18:20:29 +0100

    Added app schema

---
+++ packages/thrift-api/server/src/app-schema/reviews/products.ts
@@ -0,0 +1,20 @@
+import Joi from 'joi'
+
+export const ProductReviewRequestSchema = Joi.object({
+  order_item_id: Joi.number().required(),
+  rating: Joi.number().min(0.00).max(5.00).required(),
+  customer_remark: Joi.string().allow(null, ''),
+})
+
+export const ProductReviewResponseSchema = Joi.object({
+  order_item_id: Joi.number().required(),
+  rating: Joi.number().min(0.00).max(5.00).required(),
+  customer_id: Joi.string().uuid().required(),
+  customer_remark: Joi.string().allow(null, ''),
+  created_at: Joi.date().required(),
+  updated_at: Joi.date().required(),
+})
+
+export const ProductReviewIdSchema = Joi.object({
+  order_item_id: Joi.number().required(),
+})
