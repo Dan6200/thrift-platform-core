47f051f8737080fa36224dfda13a112cd41319d8
Daniel Nyong <danielnyong66@gmail.com>
2025-09-22 11:14:29 +0100

    Moved type guards to product tests

--- packages/thrift-api/server/src/types/products/index.ts
+++ packages/thrift-api/server/src/types/products/index.ts
@@ -1,12 +1,3 @@
-import {
-  ProductIdSchema,
-  ProductGETAllResponseSchema,
-  ProductCreateRequestSchema,
-  ProductUpdateRequestSchema,
-  ProductResponseSchema,
-  ProductGETResponseSchema,
-} from '../../app-schema/products/index.js'
-
 export type VariantOption = {
   option_id: number
   option_name: string
@@ -85,52 +76,6 @@ export type ProductMedia = MediaType & {
   is_thumbnail_image: boolean
 }
 
-export function isValidProductUpdateRequestData(
-  productData: unknown,
-): productData is ProductRequestData {
-  const { error } = ProductUpdateRequestSchema.validate(productData)
-  error && console.error(error)
-  return !error
-}
-
-export function isValidProductCreateRequestData(
-  productData: unknown,
-): productData is ProductRequestData {
-  const { error } = ProductCreateRequestSchema.validate(productData)
-  error && console.error(error)
-  return !error
-}
-
-export function isValidProductGETResponseData(
-  data: unknown,
-): data is ProductResponseData {
-  const { error } = ProductGETResponseSchema.validate(data)
-  error && console.error(error)
-  return !error
-}
-
-export function isValidProductResponseData(
-  data: unknown,
-): data is ProductResponseData {
-  const { error } = ProductResponseSchema.validate(data)
-  error && console.error(error)
-  return !error
-}
-
 export interface ProductID {
   product_id: number
-}
-
-export function isValidProductId(data: unknown): data is ProductID {
-  const { error } = ProductIdSchema.validate(data)
-  error && console.error(error)
-  return !error
-}
-
-export function isValidProductGETAllResponseData(
-  data: unknown,
-): data is ProductResponseData[] {
-  const { error } = ProductGETAllResponseSchema.validate(data)
-  error && console.error(error)
-  return !error
 }
