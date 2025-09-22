575e5e7b920dc66cff4867e62d27d51bd67d25e7
Daniel Nyong <danielnyong66@gmail.com>
2025-09-22 11:41:00 +0100

    Refactoring product test. Separating typeguards

--- packages/thrift-api/server/src/tests/integrated/products/definitions/index.ts
+++ packages/thrift-api/server/src/tests/integrated/products/definitions/index.ts
@@ -2,135 +2,25 @@ import chai from 'chai'
 import chaiHttp from 'chai-http'
 import { StatusCodes } from 'http-status-codes'
 import {
-  ProductIdSchema,
-  ProductGETAllResponseSchema,
-  ProductCreateRequestSchema,
-  ProductUpdateRequestSchema,
-  ProductResponseSchema,
-  ProductGETResponseSchema,
-} from '#src/app-schema/products/index.js'
-import {
-  ProductRequestData,
-  ProductResponseData,
-  ProductID,
-} from '../../../../types/products/index.js'
+  isValidProductUpdateRequestData,
+  isValidProductCreateRequestData,
+  isValidProductGETResponseData,
+  isValidProductResponseData,
+  isValidProductId,
+  isValidProductGETAllResponseData,
+} from '../../helpers/type-guards/products.js'
 import {
   TestRequestWithQParams,
   TestRequestWithQParamsAndBody,
 } from '../../test-request/types.js'
 import testRoutes from '../../test-request/index.js'
 import {
-  VariantIdResponseSchema,
-  VariantIdSchema,
-  VariantRequestSchema,
-  VariantResponseSchema,
-  VariantUpdateRequestSchema,
-} from '#src/app-schema/products/variants.js'
-import {
-  VariantId,
-  VariantIdResponse,
-  VariantRequestData,
-  VariantResponseData,
-  VariantUpdateRequestData,
-} from '#src/types/products/variants.js'
-
-export function isValidVariantId(data: unknown): data is VariantId {
-  const { error } = VariantIdSchema.validate(data)
-  if (error) {
-    console.error(error)
-    return false
-  }
-  return true
-}
-
-export function isValidVariantRequestData(
-  data: unknown,
-): data is VariantRequestData {
-  const { error } = VariantRequestSchema.validate(data)
-  if (error) {
-    console.error(error)
-    return false
-  }
-  return true
-}
-
-export function isValidVariantUpdateRequestData(
-  data: unknown,
-): data is VariantUpdateRequestData {
-  const { error } = VariantUpdateRequestSchema.validate(data)
-  if (error) {
-    console.error(error)
-    return false
-  }
-  return true
-}
-
-export function isValidVariantResponseData(
-  data: unknown,
-): data is VariantResponseData {
-  const { error } = VariantResponseSchema.validate(data)
-  if (error) {
-    console.error(error)
-    return false
-  }
-  return true
-}
-
-export function isValidVariantIdResponseData(
-  data: unknown,
-): data is VariantIdResponse {
-  const { error } = VariantIdResponseSchema.validate(data)
-  if (error) {
-    console.error(error)
-    return false
-  }
-  return true
-}
-export function isValidProductUpdateRequestData(
-  productData: unknown,
-): productData is ProductRequestData {
-  const { error } = ProductUpdateRequestSchema.validate(productData)
-  error && console.error(error)
-  return !error
-}
-
-export function isValidProductCreateRequestData(
-  productData: unknown,
-): productData is ProductRequestData {
-  const { error } = ProductCreateRequestSchema.validate(productData)
-  error && console.error(error)
-  return !error
-}
-
-export function isValidProductGETResponseData(
-  data: unknown,
-): data is ProductResponseData {
-  const { error } = ProductGETResponseSchema.validate(data)
-  error && console.error(error)
-  return !error
-}
-
-export function isValidProductResponseData(
-  data: unknown,
-): data is ProductResponseData {
-  const { error } = ProductResponseSchema.validate(data)
-  error && console.error(error)
-  return !error
-}
-
-export function isValidProductId(data: unknown): data is ProductID {
-  const { error } = ProductIdSchema.validate(data)
-  error && console.error(error)
-  return !error
-}
-
-export function isValidProductGETAllResponseData(
-  data: unknown,
-): data is ProductResponseData[] {
-  const { error } = ProductGETAllResponseSchema.validate(data)
-  error && console.error(error)
-  return !error
-}
+  isValidVariantId,
+  isValidVariantRequestData,
+  isValidVariantUpdateRequestData,
+  isValidVariantResponseData,
+  isValidVariantIdResponseData,
+} from '../../helpers/type-guards/variants.js'
 
 chai.use(chaiHttp).should()
 
@@ -193,4 +83,3 @@ export const testDeleteVariant = (<TestRequestWithQParams>testRoutes)({
   verb: 'delete',
   validateTestResData: null,
 })
-
