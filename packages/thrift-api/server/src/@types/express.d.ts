1269a818d1cbd6d87dae77ca047f9016c6d1996a
Daniel Nyong <danielnyong66@gmail.com>
2025-09-23 21:33:46 +0100

    Moved declaration merge file

---
+++ packages/thrift-api/server/src/@types/express.d.ts
@@ -0,0 +1,14 @@
+import { Request } from 'express'
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string
+      validatedParams?: Record<string, string>
+      validatedQueryParams?: Record<string, any>
+      validatedBody?: Record<string, any>
+      dbResult?: any[] // This will hold the result from the DB query middleware
+      validatedResponse?: any[] // This will hold the validated response data before sending
+    }
+  }
+}
