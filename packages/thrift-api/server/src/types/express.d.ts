3608cf8236f4a9a50c724fccd7fa625b8e4dd697
Daniel Nyong <danielnyong66@gmail.com>
2025-09-23 09:32:50 +0100

    Create declaration merge file

--- packages/thrift-api/server/src/types/express.d.ts
+++ packages/thrift-api/server/src/types/express.d.ts
@@ -1,12 +1,10 @@
-import { QueryParams } from './process-routes.js'
-
 declare global {
   namespace Express {
     interface Request {
       userId?: string
-      validatedParams?: QueryParams['params']
-      validatedQueryParams?: QueryParams['query']
-      validatedBody?: QueryParams['body']
+      validatedParams?: Record<string, string>
+      validatedQueryParams?: Record<string, any>
+      validatedBody?: Record<string, any>
       dbResult?: any // This will hold the result from the DB query middleware
       validatedResponse?: any // This will hold the validated response data before sending
     }
