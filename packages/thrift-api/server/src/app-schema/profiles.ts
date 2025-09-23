bbe701ceb977d51fd4c8d703ca2e7fe3de7d4fb8
Daniel Nyong <danielnyong66@gmail.com>
2025-09-23 21:49:22 +0100

    tests pass for profiles

--- packages/thrift-api/server/src/app-schema/profiles.ts
+++ packages/thrift-api/server/src/app-schema/profiles.ts
@@ -3,35 +3,30 @@
 import joi from 'joi'
 
 export const ProfileRequestSchema = joi
-  .array()
-  .items(
-    joi
-      .object()
-      .keys({
-        first_name: joi.string().alphanum().min(3).max(30).required(),
-        last_name: joi.string().alphanum().min(3).max(30).required(),
-        email: joi
-          .string()
-          .pattern(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-          )
-          .allow(null),
-        phone: joi
-          .string()
-          .pattern(
-            /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
-          )
-          .allow(null),
-        // password: joi.string(),
-        dob: joi.date().required(),
-        country: joi.string(),
-        is_customer: joi.boolean().required(),
-        is_vendor: joi.boolean().required(),
-      })
-      .or('email', 'phone')
-      .required(),
-  )
-  .length(1)
+  .object()
+  .keys({
+    first_name: joi.string().alphanum().min(3).max(30).required(),
+    last_name: joi.string().alphanum().min(3).max(30).required(),
+    email: joi
+      .string()
+      .pattern(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      )
+      .allow(null),
+    phone: joi
+      .string()
+      .pattern(
+        /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+      )
+      .allow(null),
+    // password: joi.string(),
+    dob: joi.date().required(),
+    country: joi.string(),
+    is_customer: joi.boolean().required(),
+    is_vendor: joi.boolean().required(),
+  })
+  .or('email', 'phone')
+  .required()
 /*
     password: joi
       .string()
@@ -40,76 +35,59 @@ export const ProfileRequestSchema = joi
       ),*/
 
 export const ProfileResponseSchema = joi
-  .array()
-  .items(
-    joi
-      .object()
-      .keys({
-        id: joi.string().guid({ version: 'uuidv4' }),
-        first_name: joi.string().alphanum().min(3).max(30).required(),
-        last_name: joi.string().alphanum().min(3).max(30).required(),
-        email: joi
-          .string()
-          .pattern(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-          )
-          .allow(null),
-        phone: joi
-          .string()
-          .pattern(
-            /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
-          )
-          .allow(null),
-        dob: joi
-          .alternatives()
-          .try(joi.date().required(), joi.string().required()),
-        country: joi.string().required(),
-        is_customer: joi.boolean().required(),
-        is_vendor: joi.boolean().required(),
-        created_at: joi.date().required(),
-        updated_at: joi.date().required(),
-        deleted_at: joi.date().allow(null),
-      })
-      .or('email', 'phone')
-      .required(),
-  )
-  .length(1)
+  .object()
+  .keys({
+    id: joi.string().guid({ version: 'uuidv4' }),
+    first_name: joi.string().alphanum().min(3).max(30).required(),
+    last_name: joi.string().alphanum().min(3).max(30).required(),
+    email: joi
+      .string()
+      .pattern(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      )
+      .allow(null),
+    phone: joi
+      .string()
+      .pattern(
+        /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+      )
+      .allow(null),
+    dob: joi.alternatives().try(joi.date().required(), joi.string().required()),
+    country: joi.string().required(),
+    is_customer: joi.boolean().required(),
+    is_vendor: joi.boolean().required(),
+    created_at: joi.date().required(),
+    updated_at: joi.date().required(),
+    deleted_at: joi.date().allow(null),
+  })
+  .or('email', 'phone')
+  .required()
 
 export const ProfileUpdateRequestSchema = joi
-  .array()
-  .items(
-    joi
-      .object({
-        first_name: joi.string().alphanum().min(3).max(30),
-        last_name: joi.string().alphanum().min(3).max(30),
-        email: joi
-          .string()
-          .pattern(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-          )
-          .allow(null),
-        phone: joi
-          .string()
-          .pattern(
-            /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
-          )
-          .allow(null),
-        dob: joi.alternatives().try(joi.date(), joi.string()),
-        country: joi.string(),
-        is_customer: joi.boolean().optional(),
-        is_vendor: joi.boolean().optional(),
-      })
-      .required(),
-  )
-  .length(1)
+  .object({
+    first_name: joi.string().alphanum().min(3).max(30),
+    last_name: joi.string().alphanum().min(3).max(30),
+    email: joi
+      .string()
+      .pattern(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      )
+      .allow(null),
+    phone: joi
+      .string()
+      .pattern(
+        /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+      )
+      .allow(null),
+    dob: joi.alternatives().try(joi.date(), joi.string()),
+    country: joi.string(),
+    is_customer: joi.boolean().optional(),
+    is_vendor: joi.boolean().optional(),
+  })
+  .required()
 
 export const ProfileIDSchema = joi
-  .array()
-  .items(
-    joi
-      .object({
-        id: joi.string().guid({ version: 'uuidv4' }),
-      })
-      .required(),
-  )
-  .length(1)
+  .object({
+    id: joi.string().guid({ version: 'uuidv4' }),
+  })
+  .required()
