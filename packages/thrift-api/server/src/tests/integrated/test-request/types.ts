055b5177b5a2ec1cfb6541144f05780c6d00c10a
Daniel Nyong <danielnyong66@gmail.com>
2025-09-24 10:26:54 +0100

    Made types more flexible

--- packages/thrift-api/server/src/tests/integrated/test-request/types.ts
+++ packages/thrift-api/server/src/tests/integrated/test-request/types.ts
@@ -21,35 +21,11 @@ export type TestRequest = (
   testRequestParams: TestRequestParams,
 ) => TestRequestOuter
 
-export type TestRequestWithQParamsAndBody = (
-  testRequestParams: TestRequestParams & {
-    validateTestReqData: (data: unknown) => boolean
-  },
-) => TestRequestOuterWQueryNBody
-
-export type TestRequestWithQParams = (
-  testRequestParams: TestRequestParams,
-) => TestRequestOuterWQuery
-
-export type TestRequestWithBody = (
-  testRequestParams: TestRequestParams & {
-    validateTestReqData: (data: unknown) => boolean
-  },
-) => TestRequestOuterWBody
-
-type TestRequestOuterWBody = (requestParams: RequestParams) => Promise<any>
-
-type TestRequestOuterWQuery = (requestParams: RequestParams) => Promise<any>
-
-type TestRequestOuterWQueryNBody = (
-  requestParams: RequestParams,
-) => Promise<any>
-
 type TestRequestOuter = (requestParams: RequestParams) => Promise<any>
 
 export type RequestParams = {
   params?: { [k: string]: any }
   query?: { [k: string]: any }
   body?: any | any[]
-  token: string
+  token?: string
 }
