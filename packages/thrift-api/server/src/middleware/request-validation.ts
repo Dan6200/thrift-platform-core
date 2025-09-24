ae0faf90f4748b793acb708ecf8c2746d3072d29
Daniel Nyong <danielnyong66@gmail.com>
2025-09-24 08:42:54 +0100

    Updated request validation

--- packages/thrift-api/server/src/middleware/request-validation.ts
+++ packages/thrift-api/server/src/middleware/request-validation.ts
@@ -1,33 +1,38 @@
 import { Request, Response, NextFunction } from 'express'
 import Joi from 'joi'
 import BadRequestError from '../errors/bad-request.js'
+import logger from '#src/utils/logger.js'
 
 // A middleware factory to validate different parts of the request
-export const validate = (schema: Joi.ObjectSchema) => (
-  req: Request,
-  _res: Response,
-  next: NextFunction,
-) => {
-  const { error, value } = schema.validate(
-    {
-      body: req.body,
-      query: req.query,
-      params: req.params,
-    },
-    { abortEarly: false },
-  ) // abortEarly: false shows all validation errors
+export const validate =
+  (schema: Joi.ObjectSchema) =>
+  (req: Request, _res: Response, next: NextFunction) => {
+    let input = {}
+    if (Object.entries(req.body).length !== 0) input = { body: req.body }
+    if (Object.entries(req.query).length !== 0)
+      input = { ...input, query: req.query }
+    if (Object.entries(req.params).length !== 0)
+      input = { ...input, params: req.params }
+    const { error, value } = schema.validate(
+      {
+        ...input, // avoid passing wrong req fields
+      },
+      { abortEarly: false },
+    ) // abortEarly: false shows all validation errors
 
-  if (error) {
-    // If validation fails, throw a BadRequestError
-    throw new BadRequestError(
-      'Validation failed: ' + error.details.map((detail) => detail.message).join('; '),
-    )
+    if (error) {
+      logger.error(error)
+      // If validation fails, throw a BadRequestError
+      throw new BadRequestError(
+        'Validation failed: ' +
+          error.details.map((detail) => detail.message).join('; '),
+      )
+    }
+
+    // If validation is successful, set the validated data on the request object
+    req.validatedBody = value.body
+    req.validatedQueryParams = value.query
+    req.validatedParams = value.params
+
+    next()
   }
-
-  // If validation is successful, set the validated data on the request object
-  req.validatedBody = value.body
-  req.validatedQueryParams = value.query
-  req.validatedParams = value.params
-
-  next()
-}
