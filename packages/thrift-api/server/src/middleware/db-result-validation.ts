428dc3b1218e7b93990203f418813a5af89139a2
Daniel Nyong <danielnyong66@gmail.com>
2025-09-24 10:08:06 +0100

    return 404 if GET returns empty

--- packages/thrift-api/server/src/middleware/db-result-validation.ts
+++ packages/thrift-api/server/src/middleware/db-result-validation.ts
@@ -2,12 +2,15 @@ import { Request, Response, NextFunction } from 'express'
 import Joi from 'joi'
 import InternalServerError from '../errors/internal-server.js'
 import logger from '#src/utils/logger.js'
+import NotFoundError from '#src/errors/not-found.js'
 
 export const validateDbResult = (
   schema: Joi.ObjectSchema | Joi.ArraySchema,
 ) => {
   return (req: Request, _res: Response, next: NextFunction) => {
     if (!req.dbResult) {
+      if (req.method === 'GET')
+        throw new NotFoundError('Requested resource was not found')
       throw new InternalServerError(
         'Database result not found on request object',
       )
