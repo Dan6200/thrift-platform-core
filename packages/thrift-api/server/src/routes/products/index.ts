ec5418891263dba66a40b4e118fe0dfc077a4126
Daniel Nyong <danielnyong66@gmail.com>
2025-09-25 21:41:01 +0100

    Rewritten in new middleware composition style

--- packages/thrift-api/server/src/routes/products/index.ts
+++ packages/thrift-api/server/src/routes/products/index.ts
@@ -1,30 +1,73 @@
 import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { validate } from '#src/request-validation.js'
+import { validateDbResult } from '#src/db-result-validation.js'
+import { sendResponse } from '#src/send-response.js'
+import authenticateUser from '#src/authentication.js'
+import { hasStoreAccess } from '#src/authorization/has-store-access.js'
 import {
-  createProduct,
-  getAllProducts,
-  getProduct,
-  updateProduct,
-  deleteProduct,
-} from '../../controllers/products/index.js'
+  createProductLogic,
+  getAllProductsLogic,
+  getProductLogic,
+  updateProductLogic,
+  deleteProductLogic,
+} from '#src/logic/products/index.js'
 import {
-  createVariant,
-  updateVariant,
-  deleteVariant,
-} from '../../controllers/products/variants/index.js'
+  ProductCreateRequestSchema,
+  ProductGetAllRequestSchema,
+  ProductGetRequestSchema,
+  ProductUpdateRequestSchema,
+  ProductDeleteRequestSchema,
+  ProductGETAllResponseSchema,
+  ProductGETResponseSchema,
+  ProductResponseSchema,
+} from '#src/app-schema/products/index.js'
 
 const router = express.Router({ mergeParams: true })
+const { CREATED, OK, NO_CONTENT } = StatusCodes
 
-router.route('/').post(createProduct).get(getAllProducts)
+router
+  .route('/')
+  .post(
+    authenticateUser,
+    validate(ProductCreateRequestSchema),
+    hasStoreAccess(['admin', 'editor']),
+    createProductLogic,
+    validateDbResult(ProductResponseSchema),
+    sendResponse(CREATED),
+  )
+  .get(
+    validate(ProductGetAllRequestSchema),
+    getAllProductsLogic,
+    validateDbResult(ProductGETAllResponseSchema),
+    sendResponse(OK),
+  )
+
 router
   .route('/:productId')
-  .get(getProduct)
-  .patch(updateProduct)
-  .delete(deleteProduct)
+  .get(
+    validate(ProductGetRequestSchema),
+    getProductLogic,
+    validateDbResult(ProductGETResponseSchema),
+    sendResponse(OK),
+  )
+  .patch(
+    authenticateUser,
+    validate(ProductUpdateRequestSchema),
+    hasStoreAccess(['admin', 'editor']),
+    updateProductLogic,
+    validateDbResult(ProductResponseSchema),
+    sendResponse(OK),
+  )
+  .delete(
+    authenticateUser,
+    validate(ProductDeleteRequestSchema),
+    hasStoreAccess(['admin']),
+    deleteProductLogic,
+    sendResponse(NO_CONTENT),
+  )
 
-router.route('/:productId/variants').post(createVariant)
-router
-    .route('/:productId/variants/:variantId')
-    .patch(updateVariant)
-    .delete(deleteVariant)
+// Variant routes will be handled separately
 
 export default router
+
