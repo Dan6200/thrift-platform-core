717a73b48f0374780b8c7631030dc4dfb795b5a6
Daniel Nyong <danielnyong66@gmail.com>
2025-09-25 23:14:06 +0000

    Removed unneeded dynamic binding array builder

--- packages/thrift-api/server/src/logic/products/get-all-products.ts
+++ packages/thrift-api/server/src/logic/products/get-all-products.ts
@@ -8,18 +8,14 @@ export const getAllProductsLogic = async (
   next: NextFunction,
 ) => {
   const { storeId, limit, sort, offset } = req.validatedQueryParams!
-  let params: (string | undefined)[] = []
   let whereClause = ''
 
   if (req.userId && storeId) {
     whereClause = `WHERE p.vendor_id=:vendorId AND p.store_id=:storeId`
-    params.push(req.userId, storeId)
   } else if (req.userId) {
     whereClause = `WHERE p.vendor_id=:vendorId`
-    params.push(req.userId)
   } else if (storeId) {
     whereClause = `WHERE p.store_id=:storeId`
-    params.push(storeId)
   }
 
   let dbQueryString = `
