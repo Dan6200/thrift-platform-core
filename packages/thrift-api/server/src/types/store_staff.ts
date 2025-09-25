5971072c5643965ee9c40c40abebdf02e706f458
Daniel Nyong <danielnyong66@gmail.com>
2025-09-25 12:07:43 +0000

    added timestamp to type def

--- packages/thrift-api/server/src/types/store_staff.ts
+++ packages/thrift-api/server/src/types/store_staff.ts
@@ -1,5 +1,7 @@
 export interface StoreStaff {
-  store_id: number;
-  staff_id: string; // uuid
-  role: 'admin' | 'editor' | 'viewer';
+  store_id: number
+  staff_id: string // uuid
+  role: 'admin' | 'editor' | 'viewer'
+  created_at?: Date
+  updated_at?: Date
 }
