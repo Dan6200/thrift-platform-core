edfc1bb16671ae8cc213c110d936ccb807a722dd
Daniel Nyong <danielnyong66@gmail.com>
2025-09-25 12:10:11 +0000

    Deprecated type guards for store_staff

--- packages/thrift-api/server/src/tests/integrated/helpers/type-guards/store-staff-deprecated.ts
+++ packages/thrift-api/server/src/tests/integrated/helpers/type-guards/store-staff-deprecated.ts
@@ -1,15 +1,15 @@
 import {
-  AddStoreStaffSchema,
-  UpdateStoreStaffSchema,
+  // AddStoreStaffSchema,
+  // UpdateStoreStaffSchema,
   StoreStaffListResponseSchema,
   StoreStaffResponseSchema,
 } from '#src/app-schema/store_staff.js'
 
-export const isValidAddStoreStaffRequest = (data: unknown) =>
-  AddStoreStaffSchema.validate(data).error === undefined
+// export const isValidAddStoreStaffRequest = (data: unknown) =>
+//   AddStoreStaffSchema.validate(data).error === undefined
 
-export const isValidUpdateStoreStaffRequest = (data: unknown) =>
-  UpdateStoreStaffSchema.validate(data).error === undefined
+// export const isValidUpdateStoreStaffRequest = (data: unknown) =>
+//   UpdateStoreStaffSchema.validate(data).error === undefined
 
 export const isValidStoreStaffListResponse = (data: unknown) =>
   StoreStaffListResponseSchema.validate(data).error === undefined
