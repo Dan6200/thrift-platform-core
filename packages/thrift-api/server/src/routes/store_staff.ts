97a183fe6fcac43be8f258ae079db7bb031eec29
Daniel Nyong <danielnyong66@gmail.com>
2025-09-25 11:47:39 +0000

    refactored routes to use logic middleware

--- packages/thrift-api/server/src/routes/store_staff.ts
+++ packages/thrift-api/server/src/routes/store_staff.ts
@@ -1,14 +1,65 @@
 import { Router } from 'express'
-import { addStaff, listStaff, updateStaff, removeStaff } from '#src/controllers/store_staff/index.js'
+import { StatusCodes } from 'http-status-codes'
+import { validate } from '../request-validation.js'
+import { validateDbResult } from '../db-result-validation.js'
+import { sendResponse } from '../send-response.js'
+import authenticateUser from '../authentication.js'
+import { hasStoreAccess } from '../authorization/has-store-access.js'
+import {
+  addStaffLogic,
+  listStaffLogic,
+  updateStaffLogic,
+  removeStaffLogic,
+} from '../logic/store_staff/index.js'
+import {
+  AddStoreStaffRequestSchema,
+  ListStoreStaffRequestSchema,
+  UpdateStoreStaffRequestSchema,
+  RemoveStoreStaffRequestSchema,
+  StoreStaffResponseSchema,
+  StoreStaffListResponseSchema,
+  RemoveStoreStaffResponseSchema as RemoveStaffResponse,
+} from '../app-schema/store_staff.js'
 
 const router = Router()
+const { CREATED, OK, NO_CONTENT } = StatusCodes
 
-router.route('/:storeId/staff')
-  .post(addStaff)
-  .get(listStaff)
+router
+  .route('/:storeId/staff')
+  .post(
+    authenticateUser,
+    hasStoreAccess(['admin']),
+    validate(AddStoreStaffRequestSchema),
+    addStaffLogic,
+    validateDbResult(StoreStaffResponseSchema),
+    sendResponse(CREATED),
+  )
+  .get(
+    authenticateUser,
+    hasStoreAccess(['admin', 'editor', 'viewer']),
+    validate(ListStoreStaffRequestSchema),
+    listStaffLogic,
+    validateDbResult(StoreStaffListResponseSchema),
+    sendResponse(OK),
+  )
 
-router.route('/:storeId/staff/:staffId')
-  .put(updateStaff)
-  .delete(removeStaff)
+router
+  .route('/:storeId/staff/:staffId')
+  .patch(
+    authenticateUser,
+    hasStoreAccess(['admin']),
+    validate(UpdateStoreStaffRequestSchema),
+    updateStaffLogic,
+    validateDbResult(StoreStaffResponseSchema),
+    sendResponse(OK),
+  )
+  .delete(
+    authenticateUser,
+    hasStoreAccess(['admin']),
+    validate(RemoveStoreStaffRequestSchema),
+    removeStaffLogic,
+    validateDbResult(RemoveStaffResponse),
+    sendResponse(NO_CONTENT),
+  )
 
 export default router
