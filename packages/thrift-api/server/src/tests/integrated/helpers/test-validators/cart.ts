0089c1e159298395560724baed0b37cd539c6bdb
Daniel Nyong <danielnyong66@gmail.com>
2025-09-27 21:36:37 +0100

    Added test validators for /cart

---
+++ packages/thrift-api/server/src/tests/integrated/helpers/test-validators/cart.ts
@@ -0,0 +1,47 @@
+import {
+  AddItemToCartRequestSchema,
+  UpdateCartItemRequestSchema,
+  RemoveCartItemRequestSchema,
+  GetCartRequestSchema,
+  cartResponseSchema,
+  cartItemsSchema,
+} from '#src/app-schema/cart.js'
+import { validateTestData } from '../test-validators.js'
+
+export const validateAddItemToCartReq = (data: unknown) =>
+  validateTestData(
+    AddItemToCartRequestSchema,
+    data,
+    'Add Item to Cart Request Validation Error',
+  )
+
+export const validateUpdateCartItemReq = (data: unknown) =>
+  validateTestData(
+    UpdateCartItemRequestSchema,
+    data,
+    'Update Cart Item Request Validation Error',
+  )
+
+export const validateRemoveCartItemReq = (data: unknown) =>
+  validateTestData(
+    RemoveCartItemRequestSchema,
+    data,
+    'Remove Cart Item Request Validation Error',
+  )
+
+export const validateGetCartReq = (data: unknown) =>
+  validateTestData(
+    GetCartRequestSchema,
+    data,
+    'Get Cart Request Validation Error',
+  )
+
+export const validateCartRes = (data: unknown) =>
+  validateTestData(cartResponseSchema, data, 'Cart Response Validation Error')
+
+export const validateCartItemsRes = (data: unknown) =>
+  validateTestData(
+    cartItemsSchema,
+    data,
+    'Cart Items Response Validation Error',
+  )
