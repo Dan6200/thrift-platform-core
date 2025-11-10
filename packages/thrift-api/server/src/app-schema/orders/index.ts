77a2effa6e74abba5c4502ce4058b60e7ba806a0
Daniel Nyong <danielnyong66@gmail.com>
2025-11-10 17:49:22 +0100

    refactored for consistency

--- packages/thrift-api/server/src/app-schema/orders/index.ts
+++ packages/thrift-api/server/src/app-schema/orders/index.ts
@@ -1,15 +1,52 @@
 import Joi from 'joi'
 
-export const OrderItemRequestSchema = Joi.object({
+// Base Schemas
+export const OrderItemDataSchema = Joi.object({
   variant_id: Joi.number().integer().positive().required(),
   quantity: Joi.number().integer().positive().required(),
 })
 
-export const OrderCreateRequestSchema = Joi.object({
+export const OrderDataSchema = Joi.object({
   delivery_info_id: Joi.number().integer().positive().allow(null),
-  items: Joi.array().items(OrderItemRequestSchema).min(1).required(),
+  items: Joi.array().items(OrderItemDataSchema).min(1).required(),
 })
 
+// Request Schemas
+export const CreateOrderRequestSchema = Joi.object({
+  body: OrderDataSchema.required(),
+  query: Joi.object({
+    store_id: Joi.number().integer().positive().required(),
+  }).required(),
+  params: Joi.object().optional(),
+})
+
+export const GetOrderRequestSchema = Joi.object({
+  body: Joi.object().optional(),
+  params: Joi.object({
+    order_id: Joi.number().integer().positive().required(),
+  }).required(),
+  query: Joi.object().optional(),
+})
+
+export const GetAllOrdersRequestSchema = Joi.object({
+  body: Joi.object().optional(),
+  params: Joi.object().optional(),
+  query: Joi.object({
+    store_id: Joi.number().integer().positive().required(),
+  }).required(),
+})
+
+export const UpdateOrderRequestSchema = Joi.object({
+  body: OrderDataSchema.required(),
+  params: Joi.object({
+    order_id: Joi.number().integer().positive().required(),
+  }).required(),
+  query: Joi.object().optional(),
+})
+
+export const DeleteOrderRequestSchema = GetOrderRequestSchema
+
+// Response Schemas
 export const OrderResponseSchema = Joi.object({
   order_id: Joi.number().integer().positive().required(),
   customer_id: Joi.string().uuid().required(),
@@ -35,14 +72,4 @@ export const OrderResponseSchema = Joi.object({
     .required(),
 })
 
-export const OrderGETResponseSchema = OrderResponseSchema
-
 export const OrderGETAllResponseSchema = Joi.array().items(OrderResponseSchema)
-
-export const OrderIdSchema = Joi.object({
-  order_id: Joi.number().integer().positive().required(),
-})
-
-export const OrderQuerySchema = Joi.object({
-  store_id: Joi.number().integer().positive().required(),
-})
--- packages/thrift-api/server/src/app-schema/reviews/products.ts
+++ packages/thrift-api/server/src/app-schema/reviews/products.ts
@@ -1,11 +1,33 @@
 import Joi from 'joi'
 
-export const ProductReviewRequestSchema = Joi.object({
-  order_item_id: Joi.number().integer().positive().required(),
+// Base Schemas
+export const ProductReviewDataSchema = Joi.object({
   rating: Joi.number().min(0.0).max(5.0).required(),
   customer_remark: Joi.string().allow(null, ''),
 })
 
+// Request Schemas
+export const CreateProductReviewRequestSchema = Joi.object({
+  body: ProductReviewDataSchema.required(),
+  params: Joi.object({
+    order_item_id: Joi.number().integer().positive().required(),
+  }).required(),
+  query: Joi.object().optional(),
+})
+
+export const UpdateProductReviewRequestSchema = CreateProductReviewRequestSchema
+
+export const GetProductReviewRequestSchema = Joi.object({
+  body: Joi.object().optional(),
+  params: Joi.object({
+    order_item_id: Joi.number().integer().positive().required(),
+  }).required(),
+  query: Joi.object().optional(),
+})
+
+export const DeleteProductReviewRequestSchema = GetProductReviewRequestSchema
+
+// Response Schemas
 export const ProductReviewResponseSchema = Joi.object({
   order_item_id: Joi.number().integer().positive().required(),
   rating: Joi.number().min(0.0).max(5.0).required(),
@@ -15,14 +37,6 @@ export const ProductReviewResponseSchema = Joi.object({
   updated_at: Joi.date().iso().required(),
 })
 
-export const ProductReviewGETResponseSchema = ProductReviewResponseSchema
-
 export const ProductReviewGETAllResponseSchema = Joi.array().items(
   ProductReviewResponseSchema,
 )
-
-export const ProductReviewIdSchema = Joi.object({
-  order_item_id: Joi.number().integer().positive().required(),
-})
-
-export const ProductReviewQuerySchema = Joi.object({})
