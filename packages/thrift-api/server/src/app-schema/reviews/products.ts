ba139c263f2d027689dc25d02ace59ee1ba40232
Daniel Nyong <danielnyong66@gmail.com>
2025-11-10 13:16:09 +0100

    updated product review schema

--- packages/thrift-api/server/src/app-schema/reviews/products.ts
+++ packages/thrift-api/server/src/app-schema/reviews/products.ts
@@ -1,20 +1,28 @@
 import Joi from 'joi'
 
 export const ProductReviewRequestSchema = Joi.object({
-  order_item_id: Joi.number().required(),
+  order_item_id: Joi.number().integer().positive().required(),
   rating: Joi.number().min(0.0).max(5.0).required(),
   customer_remark: Joi.string().allow(null, ''),
 })
 
 export const ProductReviewResponseSchema = Joi.object({
-  order_item_id: Joi.number().required(),
+  order_item_id: Joi.number().integer().positive().required(),
   rating: Joi.number().min(0.0).max(5.0).required(),
   customer_id: Joi.string().uuid().required(),
   customer_remark: Joi.string().allow(null, ''),
-  created_at: Joi.date().required(),
-  updated_at: Joi.date().required(),
+  created_at: Joi.date().iso().required(),
+  updated_at: Joi.date().iso().required(),
 })
 
+export const ProductReviewGETResponseSchema = ProductReviewResponseSchema
+
+export const ProductReviewGETAllResponseSchema = Joi.array().items(
+  ProductReviewResponseSchema,
+)
+
 export const ProductReviewIdSchema = Joi.object({
-  order_item_id: Joi.number().required(),
+  order_item_id: Joi.number().integer().positive().required(),
 })
+
+export const ProductReviewQuerySchema = Joi.object({})
