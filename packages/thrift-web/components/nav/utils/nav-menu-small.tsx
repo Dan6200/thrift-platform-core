5076d60a1fdc90cb23bc80c065d61d61f530a079
Daniel Nyong <danielnyong66@gmail.com>
2025-11-14 18:54:58 +0100

    Refactored to match theme

--- packages/thrift-web/components/nav/utils/nav-menu-small.tsx
+++ packages/thrift-web/components/nav/utils/nav-menu-small.tsx
@@ -1,5 +1,3 @@
-// cspell:ignore SwipeableDrawer
-import { SwipeableDrawer } from '@mui/material'
 import Link from 'next/link'
 import { useEffect, useRef, useState } from 'react'
 import { Menu, Search, ShoppingCart } from 'lucide-react'
@@ -10,6 +8,12 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion'
+import {
+  Drawer,
+  DrawerContent,
+  DrawerTrigger,
+  DrawerClose,
+} from '@/components/ui/drawer'
 import { PanelRightClose, UserCircle2 } from 'lucide-react'
 import { components } from './nav-components'
 import { ModeToggle } from '@/components/dark-mode-toggle'
@@ -32,17 +36,18 @@ export const NavMenuSmall = ({
   user: (UserAccount & { token: string }) | null
   setUser: SetUser
 }) => {
-  const [isOpen, toggleDrawer] = useState(false)
   const [showSearchBox, setShowSearchBox] = useState(false)
   const totalItems = useAtomValue(getTotalCountAtom)
   const searchOverLayRef = useRef<null | HTMLDivElement>(null)
   const searchRef = useRef<null | HTMLDivElement>(null)
   const toggleSearchButton = useRef<null | HTMLButtonElement>(null)
   const [show, setShow] = useState(false)
+
   useEffect(() => {
     document.addEventListener('click', hide)
-    return () => document.addEventListener('click', hide)
+    return () => document.removeEventListener('click', hide)
   }, [])
+
   const hide = (e: Event) => {
     if (
       searchRef.current &&
@@ -53,9 +58,10 @@ export const NavMenuSmall = ({
       setShowSearchBox(false)
     }
   }
+
   return (
     <div className={showSearchBox ? 'overflow-hidden' : ''}>
-      <div className="max-w-none border-b flex flex-row items-center justify-between w-full px-4 py-4 bg-background shadow-md dark:shadow-none">
+      <div className="max-w-none flex flex-row items-center justify-between w-full m-20 px-4 py-2 rounded-xl shadow-lg border border-white/20 bg-accent/10">
         <Link
           href="/"
           className={`${font?.className} text-2xl bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent font-bold`}
@@ -96,105 +102,101 @@ export const NavMenuSmall = ({
               </Button>
             </Link>
           )}
-          <Button
-            onClick={() => toggleDrawer(true)}
-            variant="outline"
-            size="icon"
-          >
-            <Menu />
-          </Button>
-        </div>
-        <SwipeableDrawer
-          anchor="right"
-          open={isOpen}
-          onClose={() => toggleDrawer(false)}
-          onOpen={() => toggleDrawer(true)}
-        >
-          <div className="container border overflow-scroll p-4 bg-background text-foreground h-full w-[70vw] sm:w-[50vw]">
-            <Button
-              onClick={() => toggleDrawer(false)}
-              variant="outline"
-              size="icon"
-              className="border-none relative"
-            >
-              <PanelRightClose />
-            </Button>
-            <Accordion type="single" collapsible className="my-8">
-              <AccordionItem value="item-1">
-                <AccordionTrigger className="hover:no-underline">
-                  Welcome{user?.token && `, ${user.first_name}`}
-                </AccordionTrigger>
-                <AccordionContent>
-                  <div className="flex flex-col space-y-3 p-4">
-                    <Link
-                      className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-primary/80 to-secondary/90 text-primary-foreground dark:from-primary/40 dark:to-secondary/50 p-6 no-underline outline-none focus:shadow-md"
-                      href="/"
-                    >
-                      <div className="mb-2 mt-4 text-lg font-medium">
-                        Thrift Commerce
-                      </div>
-                      <p className="text-sm leading-tight text-primary-foreground dark:text-primary-foreground/80">
-                        Shop new handpicked deals in categories such as
-                        electronics, computers & tablets, fashion & fashion
-                        accessories etc.
-                      </p>
-                    </Link>
-                    <Link
-                      href="/deals/new-arrivals"
-                      title="New Arrivals"
-                      className="active:bg-neutral-300 dark:active:bg-neutral-700"
-                    >
-                      New Arrivals for fashion and fashion accessories.
-                    </Link>
-                    <Link
-                      href="/deals/electronics"
-                      title="Deals on Electronics"
-                      className="active:bg-neutral-300 dark:active:bg-neutral-700"
-                    >
-                      Hot new deals on electronic items such as TVs, mobile
-                      phones and household electronics
-                    </Link>
-                    <Link
-                      href="/deals/computers"
-                      title="Computer Deals"
-                      className="active:bg-neutral-300 dark:active:bg-neutral-700"
-                    >
-                      Hot new deals on computers and computer accessories.
-                    </Link>
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-2">
-                <AccordionTrigger className="hover:no-underline">
-                  Browse Categories
-                </AccordionTrigger>
-                <AccordionContent>
-                  <ul className="flex flex-col space-y-3 p-4">
-                    {components.map((component) => (
+          <Drawer direction="right">
+            <DrawerTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Menu />
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent className="h-full w-[70vw] sm:w-[50vw] p-4 glass-effect">
+              <DrawerClose asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="border-none relative"
+                >
+                  <PanelRightClose />
+                </Button>
+              </DrawerClose>
+              <Accordion type="single" collapsible className="my-8">
+                <AccordionItem value="item-1">
+                  <AccordionTrigger className="hover:no-underline">
+                    Welcome{user?.token && `, ${user.first_name}`}
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="flex flex-col space-y-3 p-4">
                       <Link
-                        key={component.title}
-                        title={component.title}
-                        href={component.href}
+                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-primary/80 to-secondary/90 text-primary-foreground dark:from-primary/40 dark:to-secondary/50 p-6 no-underline outline-none focus:shadow-md"
+                        href="/"
+                      >
+                        <div className="mb-2 mt-4 text-lg font-medium">
+                          Thrift Commerce
+                        </div>
+                        <p className="text-sm leading-tight text-primary-foreground dark:text-primary-foreground/80">
+                          Shop new handpicked deals in categories such as
+                          electronics, computers & tablets, fashion & fashion
+                          accessories etc.
+                        </p>
+                      </Link>
+                      <Link
+                        href="/deals/new-arrivals"
+                        title="New Arrivals"
                         className="active:bg-neutral-300 dark:active:bg-neutral-700"
                       >
-                        {component.description}
+                        New Arrivals for fashion and fashion accessories.
                       </Link>
-                    ))}
-                  </ul>
-                </AccordionContent>
-              </AccordionItem>
-              <ModeToggle />
-            </Accordion>
-            {user && (
-              <Button
-                className="w-full text-destructive text-md"
-                onClick={user ? signOutWrapper.bind(null, setUser) : undefined}
-              >
-                Sign out
-              </Button>
-            )}
-          </div>
-        </SwipeableDrawer>
+                      <Link
+                        href="/deals/electronics"
+                        title="Deals on Electronics"
+                        className="active:bg-neutral-300 dark:active:bg-neutral-700"
+                      >
+                        Hot new deals on electronic items such as TVs, mobile
+                        phones and household electronics
+                      </Link>
+                      <Link
+                        href="/deals/computers"
+                        title="Computer Deals"
+                        className="active:bg-neutral-300 dark:active:bg-neutral-700"
+                      >
+                        Hot new deals on computers and computer accessories.
+                      </Link>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                  <AccordionTrigger className="hover:no-underline">
+                    Browse Categories
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <ul className="flex flex-col space-y-3 p-4">
+                      {components.map((component) => (
+                        <Link
+                          key={component.title}
+                          title={component.title}
+                          href={component.href}
+                          className="active:bg-neutral-300 dark:active:bg-neutral-700"
+                        >
+                          {component.description}
+                        </Link>
+                      ))}
+                    </ul>
+                  </AccordionContent>
+                </AccordionItem>
+                <ModeToggle />
+              </Accordion>
+              {user && (
+                <Button
+                  className="w-full text-destructive text-md"
+                  onClick={
+                    user ? signOutWrapper.bind(null, setUser) : undefined
+                  }
+                >
+                  Sign out
+                </Button>
+              )}
+            </DrawerContent>
+          </Drawer>
+        </div>
       </div>
       <div
         className={cn(
