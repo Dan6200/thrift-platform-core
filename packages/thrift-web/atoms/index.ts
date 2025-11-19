0d5f05fabcb7d869309ef64df319073529900699
Daniel Nyong <danielnyong66@gmail.com>
2025-11-19 15:52:12 +0100

    Updated UserAtom type

--- packages/thrift-web/atoms/index.ts
+++ packages/thrift-web/atoms/index.ts
@@ -1,8 +1,8 @@
 import { atom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 import { Product } from '@/types/products'
-import { UserAccount } from '@/components/user-account/types'
 import ShippingInfo from '@/types/shipping-info'
+import { User } from '@supabase/supabase-js'
 import { CardInfo } from '@/types/card-info'
 export * from './shopping-cart'
 export * from './store'
@@ -13,11 +13,8 @@ export const productAtom = atom<Product | null>(null)
 export const isSmallScreenAtom = atom(true)
 export const pageNumAtom = atom(1)
 export const cardInfo = atom<CardInfo | null>(null)
-export const userAtom = atomWithStorage<
-  (UserAccount & { token: string }) | null
->('user_account_details', null)
+export const userAtom = atom<User | null | undefined>(undefined)
 export const shippingInfoAtom = atomWithStorage<ShippingInfo | null>(
   'shipping-info',
   null,
 )
-
