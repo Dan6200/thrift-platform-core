42158329f7a0c433eea17dcc19f69c10bc679c68
Daniel Nyong <danielnyong66@gmail.com>
2025-11-20 16:39:57 +0100

    Added DAM for cloudinary

--- packages/thrift-web/components/hero.tsx
+++ packages/thrift-web/components/hero.tsx
@@ -1,5 +1,7 @@
 import { cn } from '@/lib/utils'
 import AnimatedText from './ui/animated-text'
+import { CldVideoPlayer } from 'next-cloudinary'
+import 'next-cloudinary/dist/cld-video-player.css'
 
 export function Hero({
   children,
@@ -11,15 +13,15 @@ export function Hero({
   const text = 'Create lasting memories shopping with Thrift.'
   return (
     <div className="h-screen">
-      <video
-        autoPlay
-        muted
-        loop
-        playsInline
+      <CldVideoPlayer
+        width="1920"
+        height="1080"
+        src="sellit-media/hero/laptop-unboxing-hd" // Public ID of your video
+        autoPlay="always"
+        loop={true}
+        muted={true}
         className="absolute inset-0 h-full w-full object-cover"
-        src="/laptop-unboxing-hd.mp4"
-        poster="/laptop-unboxing-hd.jpg"
-      ></video>
+      />
       <div
         className={cn(
           'text-5xl sm:text-[7rem] uppercase p-8 md:p-16 bg-foreground/60 dark:bg-background/60 top-0 absolute font-extrabold z-5 flex h-screen text-left items-start justify-center',
