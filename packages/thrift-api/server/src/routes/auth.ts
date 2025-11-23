e641364aaa05da5c042d79c99a3c80cb8c8601e6
Daniel Nyong <danielnyong66@gmail.com>
2025-11-23 21:04:05 +0100

    Moved login to client

--- packages/thrift-api/server/src/routes/auth.ts
+++ packages/thrift-api/server/src/routes/auth.ts
@@ -2,27 +2,17 @@ import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { validate } from '#src/request-validation.js'
 import { sendResponse } from '#src/send-response.js'
-import {
-  RegisterRequestSchema,
-  LoginRequestSchema,
-} from '#src/app-schema/auth.js'
-import { registerLogic, loginLogic } from '#src/logic/auth.js'
+import { RegisterRequestSchema } from '#src/app-schema/auth.js'
+import { registerLogic } from '#src/logic/auth.js'
 
 const router = express.Router()
-const { CREATED, OK } = StatusCodes
+const { CREATED } = StatusCodes
 
 router.post(
   '/register',
   validate(RegisterRequestSchema),
   registerLogic,
   sendResponse(CREATED),
-)
-
-router.post(
-  '/login',
-  validate(LoginRequestSchema),
-  loginLogic,
-  sendResponse(OK),
 )
 
 export default router
