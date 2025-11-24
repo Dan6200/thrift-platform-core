8eaa8506994e02a1be28a82a6ccec186cfb1f7f9
Daniel Nyong <danielnyong66@gmail.com>
2025-11-24 09:36:11 +0100

    moved providers to a dedicated page

--- packages/thrift-web/app/layout.tsx
+++ packages/thrift-web/app/layout.tsx
@@ -3,7 +3,6 @@
 import { Analytics } from '@vercel/analytics/react'
 import { Resizer } from '@/components/handle-resize'
 import { Nav } from '@/components/nav'
-import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
 import './globals.css'
 import Providers from './providers'
@@ -25,20 +24,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font?.className + ' h-fit w-full'}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Providers>
-            <Resizer>
-              <Nav />
-              {children}
-            </Resizer>
-            <Toaster />
-          </Providers>
-        </ThemeProvider>
+        <Providers>
+          <Resizer>
+            <Nav />
+            {children}
+          </Resizer>
+          <Toaster />
+        </Providers>
         <Analytics />
       </body>
     </html>
