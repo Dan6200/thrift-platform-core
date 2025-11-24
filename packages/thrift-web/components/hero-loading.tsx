350ecef9a186a03266f39cd862c85a5f310ea9d8
Daniel Nyong <danielnyong66@gmail.com>
2025-11-24 16:16:51 +0100

    Set up image to lazyload video

---
+++ packages/thrift-web/components/hero-loading.tsx
@@ -0,0 +1,29 @@
+// import { cn } from '@/lib/utils'
+import Image from 'next/image'
+
+export function HeroLoading({
+  children,
+}: {
+  children: React.ReactNode
+  className?: string
+}) {
+  return (
+    <div className="h-screen">
+      <Image
+        width="1920"
+        height="1080"
+        src="/laptop-unboxing-hd.jpg" // Public ID of your video
+        className="z-0 absolute inset-0 top-0 h-screen w-full object-cover"
+        alt="unboxing laptop placeholder"
+        priority={true}
+      />
+      <div
+        className={
+          'text-black-500 dark:text-foreground flex flex-col gap-16 md:gap-32 text-3xl sm:text-8xl capitalize p-8 md:p-16 bg-foreground/60 dark:bg-background/60 inset-0 top-0 absolute font-extrabold z-5 h-full text-left items-start justify-end'
+        }
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
