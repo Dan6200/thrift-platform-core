5454a2a54348aaaf6aca15c428fd365aad8d2fd9
Daniel Nyong <danielnyong66@gmail.com>
2025-11-24 09:23:57 +0100

    Added auth initializer and consolidated providers

--- packages/thrift-web/app/providers.tsx
+++ packages/thrift-web/app/providers.tsx
@@ -1,6 +1,20 @@
 'use client'
+import { ThemeProvider } from '@/components/theme-provider'
+import { AuthInitializer } from '@/auth/client/auth-context'
 import { Provider } from 'jotai'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider>{children}</Provider>
+  return (
+    <Provider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <AuthInitializer />
+        {children}
+      </ThemeProvider>
+    </Provider>
+  )
 }
