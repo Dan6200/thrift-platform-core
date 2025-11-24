96d3b83ed7e2f683388d4e9f56aabf81bf020388
Daniel Nyong <danielnyong66@gmail.com>
2025-11-24 13:36:37 +0100

    Store screensize in storage

--- packages/thrift-web/atoms/index.ts
+++ packages/thrift-web/atoms/index.ts
@@ -10,7 +10,7 @@ export * from './store'
 export const pageAtom = atom(0)
 export const productsAtom = atom<Product[]>([])
 export const productAtom = atom<Product | null>(null)
-export const isSmallScreenAtom = atom(true)
+export const isSmallScreenAtom = atomWithStorage('screen-width', true)
 export const pageNumAtom = atom(1)
 export const cardInfo = atom<CardInfo | null>(null)
 // Update userAtom to hold the Profile type
