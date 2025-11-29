aa4425c528edbfdc0b66e8435081f837e9e99a88
Daniel Nyong <danielnyong66@gmail.com>
2025-11-29 09:40:34 +0100

    Added font

--- packages/thrift-web/components/hero-loading.tsx
+++ packages/thrift-web/components/hero-loading.tsx
@@ -1,12 +1,17 @@
 // import { cn } from '@/lib/utils'
 import Image from 'next/image'
+import { Montagu_Slab } from 'next/font/google'
+
+const font = Montagu_Slab({ weight: '500', subsets: ['latin'] })
 
 export function HeroLoading({
   children,
+  className,
 }: {
   children: React.ReactNode
   className?: string
 }) {
+  const text = 'Create lasting|memories shopping|with Thrift.'
   return (
     <div className="h-screen">
       <Image
@@ -22,6 +27,9 @@ export function HeroLoading({
           'text-black-500 dark:text-foreground flex flex-col gap-16 md:gap-32 text-3xl sm:text-8xl capitalize p-8 md:p-16 bg-foreground/60 dark:bg-background/60 inset-0 top-0 absolute font-extrabold z-5 h-full text-left items-start justify-end'
         }
       >
+        <span className={'block w-full ' + font?.className}>
+          {text.split('|')}
+        </span>
         {children}
       </div>
     </div>
