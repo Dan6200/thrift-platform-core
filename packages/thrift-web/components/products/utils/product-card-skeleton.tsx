2ac497583b80190f96060dd68ec46d87e06bf94a
Daniel Nyong <danielnyong66@gmail.com>
2025-12-01 13:20:39 +0100

    updated skeleton

--- packages/thrift-web/components/products/utils/product-card-skeleton.tsx
+++ packages/thrift-web/components/products/utils/product-card-skeleton.tsx
@@ -6,16 +6,18 @@ export default function ProductCardSkeleton() {
   return (
     <Card className="h-fit flex flex-col w-full overflow-hidden rounded-sm animate-pulse">
       {/* Image Skeleton */}
-      <Skeleton className="w-full h-[10rem] sm:h-[12rem] md:h-[14rem] lg:h-[16rem]" />
+      <CardContent>
+        <Skeleton className="w-full bg-white/80 h-[10rem] sm:h-[12rem] md:h-[14rem] lg:h-[16rem]" />
 
-      <CardFooter className="p-2 sm:p-4 flex flex-1 flex-col gap-3 sm:gap-4 justify-between">
-        {/* Title Skeleton */}
-        <Skeleton className="h-5 w-4/5" />
-        {/* Ratings Skeleton */}
-        <Skeleton className="h-5 w-3/5" />
-        {/* Price Skeleton */}
-        <Skeleton className="h-7 w-2/5" />
-      </CardFooter>
+        <CardFooter className="p-2 sm:p-4 flex flex-1 flex-col gap-3 sm:gap-4 justify-between">
+          {/* Title Skeleton */}
+          <Skeleton className="h-5 w-4/5" />
+          {/* Ratings Skeleton */}
+          <Skeleton className="h-5 w-3/5" />
+          {/* Price Skeleton */}
+          <Skeleton className="h-7 w-2/5" />
+        </CardFooter>
+      </CardContent>
     </Card>
   )
 }
