d27d19d7f6902581b980475de0947ff3455d7839
Daniel Nyong <danielnyong66@gmail.com>
2025-12-19 14:57:18 +0100

    Rewrite to avoid insecure sessions in onAuthStateChanged

--- packages/thrift-web/atoms/auth-effect.ts
+++ packages/thrift-web/atoms/auth-effect.ts
@@ -1,7 +1,6 @@
 import { supabase } from '@/auth/client/config'
 import { atom } from 'jotai'
 import { userAtom } from '.'
-import { Profile } from '@/types/profile' // Import the Profile type
 
 type AuthInitArg = unknown[]
 
@@ -10,28 +9,16 @@ type CleanupFn = () => void
 export const authEffectAtom = atom<null, AuthInitArg, CleanupFn>(
   null,
   (_get, set, _arg) => {
+    const setUser = async () => {
+      const { data: userData } = await supabase.auth.getUser()
+      set(userAtom, userData.user)
+    }
+    setUser()
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange(async (_ev, session) => {
-      if (session?.user) {
-        // Fetch profile data from the 'profiles' table
-        const { data: profile, error } = await supabase
-          .from('profiles')
-          .select('*')
-          .eq('id', session.user.id)
-          .single()
-
-        if (error) {
-          console.error('Error fetching profile in authEffectAtom:', error)
-          set(userAtom, null)
-        } else if (profile) {
-          // Combine Supabase user data and profile data
-          const fullProfile: Profile = {
-            ...profile,
-            email: session.user.email || null, // Ensure email from auth.users is included
-          }
-          set(userAtom, fullProfile)
-        }
+    } = supabase.auth.onAuthStateChange((event) => {
+      if (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED') {
+        setUser()
       } else {
         // Clear profile data on logout
         set(userAtom, null)
