024216fa7f7b8cf6800725c705bb2521e0e358ef
Daniel Nyong <danielnyong66@gmail.com>
2025-12-19 14:58:56 +0100

    Switched from Profile to User type; I believe supabase flattens User object to include user_metadata

--- packages/thrift-web/atoms/index.ts
+++ packages/thrift-web/atoms/index.ts
@@ -3,7 +3,7 @@ import { atomWithStorage } from 'jotai/utils'
 import { Product } from '@/types/products'
 import ShippingInfo from '@/types/shipping-info'
 import { CardInfo } from '@/types/card-info'
-import { Profile } from '@/types/profile' // Import the Profile type
+import { User } from '@supabase/supabase-js'
 export * from './shopping-cart'
 export * from './store'
 
@@ -14,7 +14,7 @@ export const isSmallScreenAtom = atomWithStorage('screen-width', true)
 export const pageNumAtom = atom(1)
 export const cardInfo = atom<CardInfo | null>(null)
 // Update userAtom to hold the Profile type
-export const userAtom = atom<Profile | null>(null)
+export const userAtom = atom<User | null>(null)
 export const shippingInfoAtom = atomWithStorage<ShippingInfo | null>(
   'shipping-info',
   null,
